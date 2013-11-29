de5ef505996c34f609abb0cfceb2ba90aa71b8cf
Laurent <laurent.gatto@gmail.com>
2013-11-29 00:27:48 +0000

    first working version

---
+++ R/px.R
@@ -0,0 +1,130 @@
+require("RCurl")
+require("XML")
+
+.PXDataset <- setClass("PXDataset",
+                       slots = list(
+                           ## attributes
+                           id = "character",
+                           formatVersion = "character",
+                           ## Nodes
+                           Data = "XMLNode"))
+
+setMethod("show", "PXDataset",
+          function(object) {
+              cat("Object of class \"",class(object),"\"\n",sep="")
+              fls <- pxfiles(object)
+              fls <- paste0("'", fls, "'")
+              n <- length(fls)
+              cat(" Id:", object@id, "with ")
+              cat(n, "files\n")
+              cat(" ")
+              if (n < 3) {
+                  cat(paste(fls, collapse = ", "), "\n")
+              } else {
+                  cat(paste(fls[1], collapse = ", "))
+                  cat(" ... ")
+                  cat(paste(fls[n], collapse = ", "), "\n")
+                  cat(" Use 'pxfiles()' to see them all.\n")
+              }
+          })
+
+pxnodes <- function(pxdata, name, all = FALSE) {
+    if (all) {
+        ans <- names(unlist(pxdata@Data))
+        ans <- ans[grep("children", ans)]
+        ans <- gsub("\\.", "/", ans)
+        ans <- gsub("children", "", ans)
+        return(ans)
+    }
+    if (missing(name)) ans <- names(names(pxdata@Data))        
+    else ans <- names(xmlChildren(pxdata@Data[[name]]))
+    ans
+}
+
+
+setMethod("pxid", "PXDataset",
+          function(object) {
+              ans <- object@id
+              names(ans) <- NULL
+              ans
+          })
+
+setMethod("pxurl", "PXDataset",
+          function(object) {
+              p <- "//cvParam[@accession = 'PRIDE:0000411']"
+              x <- xmlAttrs(getNodeSet(doc = xx@Data, path = p)[[1]])["value"]
+              names(x) <- NULL
+              x
+          })
+
+
+setMethod("pxtax", "PXDataset",
+          function(object) {
+              p <- "//cvParam[@accession = 'MS:1001469']"
+              x <- xmlAttrs(getNodeSet(doc = xx@Data, path = p)[[1]])["value"]
+              names(x) <- NULL
+              x
+          })
+
+
+
+setMethod("pxref", "PXDataset",
+          function(object) {
+              p <- "//cvParam[@accession = 'PRIDE:0000400']"
+              x <- xmlAttrs(getNodeSet(doc = xx@Data, path = p)[[1]])["value"]
+              names(x) <- NULL
+              x
+          })
+
+
+setMethod("pxfiles", "PXDataset",
+          function(object) {
+              ftpdir <- paste0(pxurl(object), "/")
+              strsplit(getURL(ftpdir, dirlistonly = TRUE), "\n")[[1]]
+          })
+
+
+setMethod("pxget", "PXDataset",
+          function(object, list, ...) {
+              fls <- pxfiles(object)
+              url <- pxurl(object)
+              if (missing(list)) 
+                  list <- menu(fls, FALSE, paste0("Files for ", object@id))
+              if (length(list) == 1 && list == "all") {
+                  toget <- fls
+              } else {
+                  if (is.character(list)) {
+                      toget <- fls[fls %in% list]
+                  } else toget <- fls[list]
+              }
+              if (length(toget) < 1)
+                  stop("No files to download.")
+              urls <- gsub(" ", "\ ", paste0(url, "/", toget))
+              message("Downloading ", length(urls), " file",
+                      ifelse(length(urls) > 1, "s", ""))
+              for (i in 1:length(urls)) {
+                  download.file(urls[i], toget[i], ...)
+              }
+              invisible(toget)
+          })
+
+## constructor
+PXDataset <- function(pxid) {
+    url <- paste0("http://proteomecentral.proteomexchange.org/cgi/GetDataset?ID=",
+                  pxid, "&outputMode=XML&test=no")
+    ns10 <- "http://proteomexchange.googlecode.com/svn/schema/proteomeXchange-1.0.xsd"
+    ns <- ns11 <- "http://proteomexchange.googlecode.com/svn/schema/proteomeXchange-1.1.0.xsd"
+    v <- sub("\" .+$", "",  sub("^.+formatVersion=\"", "", readLines(url)[2]))
+    if (length(grep("1.0.0", v)) == 1) ns <- ns10    
+    doc <- xmlTreeParse(url)
+    pxdata <- doc[["doc"]]$children$ProteomeXchangeDataset
+    .formatVersion <- xmlAttrs(pxdata)["formatVersion"]
+    .id <- xmlAttrs(pxdata)["id"]
+    stopifnot(pxid == .id)
+    stopifnot(v == .formatVersion)
+    .PXDataset(id = .id,
+               formatVersion = .formatVersion,
+               Data = pxdata)
+}
+
+
