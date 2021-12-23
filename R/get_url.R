e3b9c21bb9a3ab9f67e04e6bffc6371b46705aee
lgatto <laurent.gatto@gmail.com>
2021-12-23 10:55:18 +0100

    update fix_issue_17

--- R/get_url.R
+++ R/get_url.R
@@ -2,13 +2,18 @@
 fix_issue_17 <- function(x) {
     make_http_url <- function(x)
         sub("^ftp://", "http://", x)
-    http_x <- make_http_url(x)
-    if (!RCurl::url.exists(http_x))
-        x2 <- sub("/data/", "-", x)
-    http_x2 <- make_http_url(x2)
-    if (!RCurl::url.exists(http_x2))
-        stop("Can't fix URL")
-    x2
+    failed_url <- c()
+    if (RCurl::url.exists(make_http_url(x))) {
+        return(x)
+    }
+    failed_url <- append(failed_url, x)
+    x <- sub("/data/", "-", x)
+    if (RCurl::url.exists(make_http_url(x))) {
+        return(x)
+    }
+    failed_url <- append(failed_url, x)
+    stop("Can't access URL(s):\n ",
+         paste(failed_url, collapse = "\n "))
 }
 
 
